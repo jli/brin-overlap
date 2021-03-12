72028a47925c321d2a0a815e71248a1ad87ecd12
john li <jli@circularly.org>
2021-03-12 10:31:36 -0500

    improve export script: figures out where to start, loads/unloads pageinspect

--- README.md
+++ README.md
@@ -13,12 +13,18 @@ FROM brin_page_items(get_raw_page('test_brin_idx',2), 'test_brin_idx')
 ORDER BY blknum;
 ```
 
-The shell script `export_brin_items.sh` runs the above query, incrementing the page numbers until the query returns an error (which should be due to requesting a non-existent page). Set env vars to configure.
+The shell script `export_brin_items.sh` runs the above query, incrementing
+the page numbers until the query returns an error (which should be due to
+requesting a non-existent page). Set env vars to configure.
+
+Note: it's expected that the script will output an error like: `ERROR: block
+number 418 is out of range for relation "<table>_idx"`. The script simply
+keeps querying for
 
 ### Parse and render output
 
 ```shell
-python3 broviz.py -i brin_export_full.csv -o broviz_$(date "+%Y%m%d_%H%M%S").svg
+python3 broviz.py -i brinitems_export.csv -o broviz_$(date "+%Y%m%d_%H%M%S").svg
 ```
 
 
--- export_brin_items.sh
+++ export_brin_items.sh
@@ -2,21 +2,40 @@
 
 set -eu -o pipefail
 
-# Set env vars PGHOST, PGUSER, and PGPASS.
+# Set env vars PGHOST, PGDB, PGUSER, PGPASSWORD, PGIDX.
 # TODO: better way of checking that vars are set...
 PGHOST=${PGHOST:-localhost}
 PGUSER=${PGUSER:-postgres}
-OUTFILE=${OUTFILE:-brin_export_full.csv}
-echo "connecting to host $PGHOST as $PGUSER w/ $PGPASS, exporting $PGIDX"
+ATTNUM=${ATTNUM:-1}
+FIRSTPAGE=${FIRSTPAGE:-0}  # first page to start searching for regular pages
+OUTFILE=${OUTFILE:-brinitems_$(date "+%Y%m%d_%H%M%S").csv}
+echo "connecting to host $PGHOST db $PGDB as $PGUSER, exporting attname $ATTNUM from $PGIDX..."
 
 tmp=$(mktemp -d tmp-export_brin_items.XX)
 trap 'rm -rf $tmp' EXIT
 
+function load_extension {
+    psql -h "$PGHOST" -d "$PGDB" -U "$PGUSER" -X -c "CREATE EXTENSION pageinspect"
+}
+
+function unload_extension {
+    psql -h "$PGHOST" -d "$PGDB" -U "$PGUSER" -X -c "DROP EXTENSION pageinspect"
+}
+
+# Return type of page number.
+function get_page_type {
+    PAGE="$1"
+    psql -h "$PGHOST" -d "$PGDB" -U "$PGUSER" --csv -t -X \
+        -c "SELECT brin_page_type(get_raw_page('$PGIDX', $PAGE))"
+}
+
+# Export page number to CSV. Returns "good" or "bad" depending on error.
 function export_page {
     PAGE="$1"
     PAGE_OUTFILE="$tmp/brin_export_$PAGE.csv"
-    if psql -h "$PGHOST" -U "$PGUSER" "password=$PGPASS" --csv -t -X \
-        -c "SELECT blknum, value FROM brin_page_items(get_raw_page('$PGIDX', $PAGE), '$PGIDX') ORDER BY blknum" \
+    # -t to suppress headers, -X to not load .psql (suppress \timing output)
+    if psql -h "$PGHOST" -d "$PGDB" -U "$PGUSER" --csv -t -X \
+        -c "SELECT blknum, value FROM brin_page_items(get_raw_page('$PGIDX', $PAGE), '$PGIDX') WHERE attnum=$ATTNUM ORDER BY blknum" \
         > "$PAGE_OUTFILE"; then
         echo "good"
     else
@@ -24,17 +43,37 @@ function export_page {
     fi
 }
 
-page=2
+echo "-> loading pageinspect..."
+load_extension
+
+# Find first normal page.
+echo "-> locating first 'regular' page..."
+page=$FIRSTPAGE
 while true; do
-    echo "exporting brin page $page..."
-    if [ "$(export_page $page)" == "bad" ]; then
-        echo "exported $page pages"
+    res=$(get_page_type "$page")
+    echo "page $page type $res."
+    if [ "$res" == "regular" ]; then
+        echo "found first regular page: $page."
         break
     fi
     ((++page))
 done
 
-echo "concatenating csvs..."
+echo "-> exporting BRIN pages..."
+while true; do
+    echo "exporting BRIN page $page..."
+    res=$(export_page "$page")
+    if [ "$res" == "bad" ]; then
+        echo "done exporting."
+        break
+    fi
+    ((++page))
+done
+
+echo "-> unloading pageinspect..."
+unload_extension
+
+echo "-> concatenating csvs..."
 (echo "blknum,value"; cat "$tmp"/*.csv) > "$OUTFILE"
 
-echo "result in $OUTFILE ✨"
+echo "=> result in $OUTFILE ✨"
