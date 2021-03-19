efe7701c638284bf95ee0f895e7733f747c7033e
john li <jli@circularly.org>
2021-03-18 20:02:34 -0400

    add -after filtering to bro_timespan_hist tool

--- bro_timespan_hist.py
+++ bro_timespan_hist.py
@@ -4,21 +4,26 @@
 """What's the distribution of the block range time span?"""
 
 import argparse
+from datetime import datetime
 from typing import Optional
-from brin_parser import BlockRange
+
 from matplotlib import pyplot as plt
 
-from brin_parser import parse_csv_file
+from brin_parser import BlockRange, parse_csv_file
 
 
 def plot_span_hist(
-    brs: list[BlockRange], outfile: Optional[str], log: bool, xlim: Optional[float], ylim: Optional[float]
+    brs: list[BlockRange],
+    outfile: Optional[str],
+    log: bool,
+    xlim: Optional[float],
+    ylim: Optional[float],
 ):
     deltas = (br.end - br.start for br in brs)
-    days = [d.total_seconds() / 3600 / 24 for d in deltas]
-    plt.hist(days, 50, log=log)
+    hours = [d.total_seconds() / 3600 for d in deltas]
+    plt.hist(hours, 50, log=log)
     plt.title("distribution of block range time spans")
-    plt.xlabel("block range end - start (days)")
+    plt.xlabel("block range end - start (hours)")
     if xlim:
         plt.xlim(0, xlim)
     if ylim:
@@ -28,14 +33,8 @@ def plot_span_hist(
 
 
 def main(args):
-    brs = parse_csv_file(args.input)
-    plot_span_hist(
-        brs,
-        args.output,
-        args.log,
-        args.xlim,
-        args.ylim
-    )
+    brs = parse_csv_file(args.input, start=args.after)
+    plot_span_hist(brs, args.output, args.log, args.xlim, args.ylim)
 
 
 #%%
@@ -43,7 +42,14 @@ if __name__ == "__main__":
     p = argparse.ArgumentParser()
     p.add_argument("-i", dest="input", required=True, help="input path to CSV")
     p.add_argument("-o", dest="output", required=True, help="output path for histogram")
-    p.add_argument("-l", dest="log", action='store_true', default=False, help="use log scale")
+    p.add_argument(
+        "-after",
+        type=datetime.fromisoformat,
+        help="Filter out BRs after this point.",
+    )
+    p.add_argument(
+        "-l", dest="log", action="store_true", default=False, help="use log scale"
+    )
     p.add_argument("-x", dest="xlim", type=float, help="set x limit")
     p.add_argument("-y", dest="ylim", type=float, help="set y limit")
     args = p.parse_args()
